7f572e9af950b6db30abc11b7cba0d5a0974e75d
Abhay singh <1995.abhaysingh@gmail.com>
2024-01-02 14:28:58 +0530

    fixed contact page

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -2,15 +2,37 @@ import React from 'react'
 import { Sidebar } from './Sidebar'
 import { Button, Col, Container, Row } from 'react-bootstrap'
 import WavingHand from './../Assest/Image/waving-hand.png'
+import { Controller, useForm } from 'react-hook-form'
+import emailjs from '@emailjs/browser'
+import 'react-toastify/dist/ReactToastify.css';
+import  {ToastContainer, toast}  from 'react-toastify'
 
 export const Contact = () => {
 
+    const {
+        control,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm()
+
     document.title = 'Contact'
 
-
+    const onSubmit = (value) => { 
+   
+        console.log(value)
+        emailjs.send('service_6bpmfh8','template_gde2k1r', value, 'H5XIUE5fTo4kOem70', {name:value.name}).then((response) => {
+            console.log('SUCCESS!', response.status, response.text);
+            toast.success('Mail sent !')
+    reset({name:"", message:"", interest:"", email:"",budget:"" })
+         }, (err) => {
+            console.log('FAILED...', err);
+         });
+      }
     return (
 
         <Container>
+            <ToastContainer/>
             <Row>
                 <Col xs lg="3" style={{ backgroundColor: "  " }}>
                     <Sidebar />
@@ -26,29 +48,114 @@ export const Contact = () => {
                                     </div>
                                 </Row>
                                 <Row className='contact-form '>
+                                    <form onSubmit={handleSubmit(onSubmit)}>
+                                        
                                     <div style={{ justifyContent: "center", padding: "20px" }}>
                                         <Container>
                                             <Row style={{ backgroundColor: "" }} className='show-up-animation'>
-                                                <Col className='contact-input-title'>Your name</Col>
-                                                <Col className='contact-input-title'>Your email</Col>
+                                                <Col className='contact-input-title'>Your name <span className='mandatory-sign'> * </span> </Col>
+                                                <Col className='contact-input-title'>Your email <span className='mandatory-sign'> * </span> </Col>
                                             </Row>
                                             <Row style={{ backgroundColor: "" }} className='show-up-animation'>
-                                                <Col className='my-1'><input className='contact-input' type="text" placeholder='' /></Col>
-                                                <Col className='my-1'><input className='contact-input' type="text" placeholder='' /></Col>
+                                                <Col className='my-1'>
+                                                    <Controller
+                                                        name='name'
+                                                        control={control}
+                                                        rules={{ required: true }}
+                                                        render={({ field: { onChange, onBlur, value } }) => (
+                                                            <input
+                                                                className='contact-input'
+                                                                type="text"
+                                                                placeholder=''
+                                                                onChange={onChange}
+                                                                value={value}
+                                                                onBlur={onBlur} />
+                                                        )}
+                                                    />
+                                                     {/* {true && <span className='error-message'>This is required.</span>} */}
+                                                     {errors.name && <span className='error-message'>This is required.</span>}
+                                                </Col>
+
+                                                <Col className='my-1'>
+                                                    <Controller
+                                                        name='email'
+                                                        control={control}
+                                                        rules={{ required: true }}
+                                                        render={({ field: { onChange, onBlur, value } }) => (
+                                                            <input
+                                                                className='contact-input'
+                                                                type="text"
+                                                                placeholder=''
+                                                                onChange={onChange}
+                                                                value={value}
+                                                                onBlur={onBlur} />
+                                                        )}
+                                                    />
+                                                     {errors.email && <span className='error-message'>This is required.</span>}
+
+                                                </Col>
                                             </Row>
                                             <Row className='show-up-animation' style={{ backgroundColor: "", marginTop: "" }} >
-                                                <Col className='my-1 contact-input-title'>What are you interseted in?</Col>
-                                                <Col className='my-1 contact-input-title'>Project Budget</Col>
+                                                <Col className='my-1 contact-input-title'>What are you interseted in? <span className='mandatory-sign'> * </span> </Col>
+                                                <Col className='my-1 contact-input-title'>Project Budget <span className='mandatory-sign'> * </span> </Col>
                                             </Row>
                                             <Row style={{ backgroundColor: "" }}>
-                                                <Col className='my-1'><input type="text" className='contact-input' placeholder='' /></Col>
-                                                <Col className='my-1'><input type="text" className='contact-input' placeholder='' /></Col>
+                                                <Col className='my-1'>
+                                                <Controller
+                                                        name='interest'
+                                                        control={control}
+                                                        rules={{ required: true }}
+                                                        render={({ field: { onChange, onBlur, value } }) => (
+                                                            <input
+                                                                className='contact-input'
+                                                                type="text"
+                                                                placeholder=''
+                                                                onChange={onChange}
+                                                                value={value}
+                                                                onBlur={onBlur} />
+                                                        )}
+                                                    />
+                                                     {errors.interest && <span className='error-message'>This is required.</span>}
+
+                                                </Col>
+                                                <Col className='my-1'>
+                                                <Controller
+                                                        name='budget'
+                                                        control={control}
+                                                        rules={{ required: true }}
+                                                        render={({ field: { onChange, onBlur, value } }) => (
+                                                            <input
+                                                                className='contact-input'
+                                                                type="text"
+                                                                placeholder=''
+                                                                onChange={onChange}
+                                                                value={value}
+                                                                onBlur={onBlur} />
+                                                        )}
+                                                    />
+                                                     {errors.budget && <span className='error-message'>This is required.</span>}
+
+                                                </Col>
                                             </Row>
                                             <Row className='mt-1'>
                                                 <Col className='contact-input-title'>Message</Col>
                                             </Row>
                                             <Row className=''>
-                                                <Col className='my-1' style={{ backgroundColor: "", }}><textarea className='contact-input h-100' /></Col>
+                                                <Col className='my-1' style={{ backgroundColor: "", }}>
+                                                <Controller
+                                                        name='message'
+                                                        control={control}
+                                                        render={({ field: { onChange, onBlur, value } }) => (
+                                                            <textarea
+                                                                className='contact-input'
+                                                                // type="text"
+                                                                placeholder=''
+                                                                onChange={onChange}
+                                                                value={value}
+                                                                onBlur={onBlur} />
+                                                        )}
+                                                    />
+                                                    </Col>
                                             </Row>
                                             <Row className='mt-3'>
                                                 <Col className='contact-input-title'>
@@ -61,6 +168,7 @@ export const Contact = () => {
                                             </Row>
                                         </Container>
                                     </div>
+                                    </form>
                                 </Row>
                             </div>
                         </Container>
